2a94978261484262b492e377fbe2a2c48b398e19
Colleen-Cobb <colleen.cobb5@gmail.com>
2023-01-05 18:07:29 -0500

    readme and rough map

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,54 +1,3 @@
-//   // Create the base layers.
-//   var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//   })
-
-//   var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-//     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-//   });
-
-//   // Create a baseMaps object.
-//   var baseMaps = {
-//     "Street Map": street,
-//     "Topographic Map": topo
-//   };
-
-//   // Create an overlay object to hold our overlay.
-//   var overlayMaps = {
-//     Earthquakes: earthquakes
-//   };
-
-//   // Create our map, giving it the streetmap and earthquakes layers to display on load.
-//   var myMap = L.map("map", {
-//     center: [
-//       37.09, -95.71
-//     ],
-//     zoom: 5,
-//     layers: [street, earthquakes]
-//   });
-
-//   // Create a layer control.
-//   // Pass it our baseMaps and overlayMaps.
-//   // Add the layer control to the map.
-//   L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(myMap);
-
-//   var info = L.control({
-//     position: "bottomright",
-//     fillcolor: "white", 
-//     collapsed: false, 
-//     title: "Earthquake Depth"
-//   });
-
-//   // When the layer control is added, insert a div with the class of "legend".
-//   info.onAdd = function() {
-//     var div = L.DomUtil.create("div", "legend");
-//     return div;
-// };
-// // Add the info legend to the map.
-//   info.addTo(map);
-
 
 // Store our API endpoint as queryUrl.
 var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-11-01&endtime=2022-12-02&maxlongitude=180&minlongitude=-180&maxlatitude=90&minlatitude=-90";
@@ -56,10 +5,11 @@ var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function(data) {
   // Once we get a response, send the data.features object to the createFeatures function.
-  createFeatures(data.features);
-});
+  createEarthquakes(data.features);
+  });
 
-function createFeatures(earthquakeData) {
+
+function createEarthquakes(earthquakeData) {
 
   var markers = [];
 
@@ -67,67 +17,49 @@ function createFeatures(earthquakeData) {
     
     var lat = earthquakeData[i].geometry.coordinates[1]
     var lng = earthquakeData[i].geometry.coordinates[0]
-    var cord = [lat, lng]
+    var coord = [lat, lng]
     var depth = earthquakeData[i].geometry.coordinates[2]
     var mag = earthquakeData[i].properties.mag 
-    // var color = "";
-    // if (depth < 10){
-    //   color = "#77FF33"
-    // }
-    // else if (depth < 30){
-    //   color = "#33FF35"
-    // }
-    // else if (depth < 50) {
-    //   color = "#FFE033"
-    // }
-    // else if (depth < 70) {
-    //   color = "#FF8E33"
-    // }
-    // else if (depth < 90) {
-    //   color = "#FF7133"
-    // }
-    // else {
-    //   color = "#FF4A33"
-    // }
-    function chooseColor(depth) {
-      if (depth == "<10") return "yellow";
-      else if (depth <30) return "red";
-      else if (depth < 50) return "orange";
-      else if (depth <70) return "green";
-      else if (depth <90) return "purple";
-      else return "black";
+    var color = "";
+    if (depth < 10){
+      color = "#77FF33"
     }
-
+    else if (depth < 30){
+      color = "#33FF35"
+    }
+    else if (depth < 50) {
+      color = "#FFE033"
+    }
+    else if (depth < 70) {
+      color = "#FF8E33"
+    }
+    else if (depth < 90) {
+      color = "#FF7133"
+    }
+    else {
+      color = "#FF4A33"
+    } 
     markers.push(
-      L.circle(cord, {
-        collapsed: false, 
-        fillOpacity: 0.75, 
-        color: "white",
-        fillColor: chooseColor(earthquakeData[i].geometry.coordinates[2]), 
+      L.circle(coord, {
+        stroke: false,
+        fillOpacity: 0.75,
+        color: "black",
+        fillColor: color, 
+        radius: mag *10000
       }).bindPopup(`<h3>${earthquakeData[i].properties.place}</h3><hr><p>${new Date(earthquakeData[i].properties.time)}</p>`)
     )
   }
 
   var earthquakes = L.layerGroup(markers)
 
-  // Send our earthquakes layer to the createMap function/
   createMap(earthquakes);
-
-
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
-
 }
 
 function createMap(earthquakes) {
 
   // Create the base layers.
   var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
 
   var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
@@ -142,7 +74,7 @@ function createMap(earthquakes) {
 
   // Create an overlay object to hold our overlay.
   var overlayMaps = {
-    Earthquakes: earthquakes
+    "Earthquakes": earthquakes
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
@@ -163,39 +95,54 @@ function createMap(earthquakes) {
 
   function legnedColors(depth){
     if (depth < 10){
-      color = "#77FF33"
+      return "#77FF33"
     }
     else if (depth < 30){
-      color = "#33FF35"
+      return "#33FF35"
     }
     else if (depth < 50) {
-      color = "#FFE033"
+      return "#FFE033"
     }
     else if (depth < 70) {
-      color = "#FF8E33"
+      return "#FF8E33"
     }
     else if (depth < 90) {
-      color = "#FF7133"
+      return "#FF7133"
     }
     else {
-      color = "#FF4A33"
+      return "#FF4A33"
     }
   }
 
-//   var legend = L.control({
-//     position: "bottomright",
-//     fillcolor: "white", 
-//     collapsed: false, 
-//     title: "Earthquake Depth"
-//   });
+  var legend = L.control({
+    position: "bottomright",
+    fillcolor: "white", 
+    collapsed: false, 
+    title: "Earthquake Depth"
+  });
 
-//   legend.onAdd = function() {
-//     var div = L.DomUtil.create("div", "legend");
-//     var depth = [9, 29, 49, 69, 89, 500];
-//     var labels = ["<10", "10-30", "30-50", "50-70", "70-90", "90+"];
- 
-//   }
-//   return div; 
-// };
-// legend.addTo(myMap);
+
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "legend");
+    var depth = [9, 29, 49, 69, 89, 500];
+    var labels = ["<10", "10-30", "30-50", "50-70", "70-90", "90+"];
+    div.innerHTML = '<div>Earthquake Depth (Km) </div>';
+    for (var i = 0; i < depth.length; i++){
+      div.innerHTML += '<i style="background:' + legnedColors(depth[i]) + '">&nbsp;&nbsp;&nbsp;&nbsp;</i>&nbsp;'+
+                      labels[i] + '<br>';
+    }
+    return div;
+  };
+    
+  legend.addTo(myMap);
+
 }
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+![](Images/header.jpg)
 # leaflet-challenge
+
+## Overview
+
+In this repository, the user analyzed data from the United States Geological Survey (USGS). The data will serve to educate the pubulic and government organization on natrual hazard issues facing our planet, specifically, earthquakes.
+
+## Analysis
+HTML and JavaScript were utilized in this challenge in order to create the final visualization, an interactive map that represents the major earthquakes in the last 30 days. The map allows the viewer to traverse around the world on a street or topographic view and encounter circle markers that display where the earthquake occured and what time. The colors of the markers differ in the depth of the specific earthquake. A legend is located in the bottomright corner of the map that displays the earthquake depth in kilometers. 
+
+## Results
+![](Images/marker.png)
+![](Images/topo_view.png)
+![](Images/legend.png)
+
